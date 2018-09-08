6828685891992c1bf62f12aa0ebeb9aa17fbb78f
vinchens00 <it.vinogradov@yandex.ru>
2018-09-08 13:57:13 +0300

    Fetch rates from api

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-import { ratesSelectors, ratesActions } from 'ducks/rates';
+import MainPage from 'pages/Main';
 
 class App extends React.Component {
-  componentDidMount() {
-    // this.props.fetch();
-  }
   render() {
-    return <main>Main Page</main>;
+    return (
+      <main>
+        <MainPage />
+      </main>
+    );
   }
 }
 
-const Connected = connect(
-  state => ({
-    base: ratesSelectors.base(state),
-    rates: ratesSelectors.rates(state)
-  }),
-  dispatch => ({
-    fetch: bindActionCreators(ratesActions.fetch, dispatch)
-  })
-)(App);
-
-export default hot(module)(Connected);
+export default hot(module)(App);
