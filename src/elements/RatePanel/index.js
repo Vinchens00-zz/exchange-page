aaffc309b3c4b02044fe2e7f1aaf4447018c1cf9
vinchens00 <it.vinogradov@yandex.ru>
2018-09-09 16:02:53 +0300

    Fix rate styles

--- src/elements/RatePanel/index.js
+++ src/elements/RatePanel/index.js
@@ -7,13 +7,28 @@ import cn from 'utils/bem';
 import { propTypes } from './props';
 import './style.styl';
 
+const LARGE_NUMBER_COUNT = 2;
+
+const largePartRegex = new RegExp(
+  `^0*([0-9]+.{0,1}[0-9]{0,${LARGE_NUMBER_COUNT}})[0-9]*$`
+);
+
 const b = cn('rate-panel');
 
-const RatePanel = ({ className, onClick, rate, fromAssetId, toAssetId }) => (
-  <button className={b(null, null, className)} onClick={onClick}>
-    {`${ASSET_SIGN[fromAssetId]}1 = ${ASSET_SIGN[toAssetId]}${rate}`}
-  </button>
-);
+const RatePanel = ({ className, onClick, rate, fromAssetId, toAssetId }) => {
+  rate = String(rate);
+  const largeRate = rate.replace(largePartRegex, '$1');
+  const smallRate = rate.replace(largeRate, '');
+
+  return (
+    <button className={b(null, null, className)} onClick={onClick}>
+      <span className={b('large-part')}>
+        {`${ASSET_SIGN[fromAssetId]}1 = ${ASSET_SIGN[toAssetId]}${largeRate}`}
+      </span>
+      <span className={b('small-part')}>{smallRate}</span>
+    </button>
+  );
+};
 
 RatePanel.propTypes = propTypes;
 
